5d5f2be11308e88afacdfe32b5a71f874a9883ce
OM Miles <miles@ommiles.com>
2021-05-19 17:06:03 -0700

    lab complete

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,47 @@
 class Owner
-  # code goes here
+
+  @@all=[]
+  # @cats=[]
+
+  attr_reader :name, :species
+
+  def initialize(name)
+    @name=name
+    @species="human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all=[]
+  end
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+    self.cats<<cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+    self.dogs<<dog
+  end
 end
